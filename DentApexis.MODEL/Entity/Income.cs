b85c66e60e2b326c2bce61c5c8353ba5ed67e495
oguzkose <oguzkose2019@outlook.com>
2020-03-02 16:59:39 +0300

    sgdz

---
+++ DentApexis.MODEL/Entity/Income.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentApexis.MODEL.Entity
+{
+    public class Income:BaseEntity
+    {
+        public int ID { get; set;}
+        public bool isPaid { get; set; }
+        public decimal Payment { get; set; }
+       
+        public DateTime IncomePeriod { get; set; }//gelir dönemi
+
+    }
+}
